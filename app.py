a3d64bb1e4d1e2e2502be6041eda69fc7ce1970d
AbdullahMaqbool22 <abdullahmaqbool08@gmail.com>
2025-12-15 18:46:45 +0500

    CI/CD Jenkins Project

--- app.py
+++ app.py
@@ -15,4 +15,26 @@ def init_db():
 def items():
     conn = sqlite3.connect(DB_FILE)
     cursor = conn.cursor()
-    if request.method ==
+    
+    if request.method == 'POST':
+        data = request.get_json()
+        name = data.get('name')
+        cursor.execute('INSERT INTO items (name) VALUES (?)', (name,))
+        conn.commit()
+        conn.close()
+        return jsonify({"message": "Item added successfully"}), 201
+    
+    elif request.method == 'GET':
+        cursor.execute('SELECT * FROM items')
+        rows = cursor.fetchall()
+        conn.close()
+        items_list = [{"id": row[0], "name": row[1]} for row in rows]
+        return jsonify(items_list), 200
+
+@app.route('/')
+def home():
+    return jsonify({"message": "Welcome to Python Web App with SQLite!"}), 200
+
+if __name__ == '__main__':
+    init_db()
+    app.run(host='0.0.0.0', port=5000)
